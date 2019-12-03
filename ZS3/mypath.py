553a7a8aad605d110bf532b944143d9ef5687e67
maximebucher <maxime.bucher@valeo.com>
2019-12-03 17:51:14 +0100

    readme

--- ZS3/mypath.py
+++ ZS3/mypath.py
@@ -1,17 +1,13 @@
-import valeodata
 
 class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            #return str(valeodata.download('zsd_pascalVOC/VOC2012'))
-            return '/datasets_local/zsd_pascalVOC/VOC2012'
+            return 'data/VOC2012'
         elif dataset == 'sbd':
-            #return str(valeodata.download('zsd_pascalVOC/VOC2012/benchmark_RELEASE'))
-            return '/datasets_local/zsd_pascalVOC/VOC2012/benchmark_RELEASE'
+            return 'data/VOC2012/benchmark_RELEASE'
         elif dataset == 'context':
-            valeodata.download('zsd_segmentation_pascalVOC/datasets/context')
-            return '/datasets_local/zsd_segmentation_pascalVOC/datasets/context'
+            return 'data/context/'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
