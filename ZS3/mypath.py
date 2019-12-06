8a7489e8e8e92559c9a0380be3e8f14f9948dab7
maximebucher <maxime.bucher@valeo.com>
2019-12-06 15:25:00 +0100

    readme

--- ZS3/mypath.py
+++ ZS3/mypath.py
@@ -4,14 +4,11 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            #return 'data/VOC2012'
-            return '/datasets_local/zsd_pascalVOC/VOC2012'
+            return 'data/VOC2012'
         elif dataset == 'sbd':
-            #return 'data/VOC2012/benchmark_RELEASE'
-            return '/datasets_local/zsd_pascalVOC/VOC2012/benchmark_RELEASE'
+            return 'data/VOC2012/benchmark_RELEASE'
         elif dataset == 'context':
-            #return 'data/context/'
-            return '/datasets_local/zsd_segmentation_pascalVOC/datasets/context'
+            return 'data/context/'
         else:
             print('Dataset {} not available.'.format(dataset))
             raise NotImplementedError
