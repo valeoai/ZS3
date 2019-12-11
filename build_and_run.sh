11daef9b2a28a0a03cd1a5e6e15e6c9c4ec1af71
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-12-11 16:32:25 +0000

    Removed the build_and_run.

--- build_and_run.sh
+++
@@ -1,8 +0,0 @@
-sudo bash -c "DOCKER_BUILDKIT=1 docker build -t zs3_test ."
-sudo docker run --rm --gpus "device=4" \
-                -v /media/data/datasets/zsd_pascalVOC/VOC2012:/ZS3/zs3/data/VOC2012:ro \
-                -v /mnt/data/workspace/mbucher/zs3_temp/ZS3/checkpoint:/ZS3/zs3/checkpoint:ro \
-                -e DRY_RUN=1 \
-                --ipc=host \
-                zs3_test \
-                bash -c "python ./train_pascal.py"
