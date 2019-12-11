62c604bbb9c82d8ba18fef46bf8d3684816f9c44
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2019-12-11 14:42:52 +0000

    Grouped some training loop.

--- zs3/train_context.py
+++ zs3/train_context.py
@@ -16,6 +16,7 @@ from zs3.utils.saver import Saver
 from zs3.utils.summaries import TensorboardSummary
 from zs3.parsing import get_parser
 from zs3.exp_data import CLASSES_NAMES
+from zs3.common_stuff import do_training
 
 
 class Trainer:
@@ -112,58 +113,7 @@ class Trainer:
             args.start_epoch = 0
 
     def training(self, epoch):
-        train_loss = 0.0
-        self.model.train()
-        tbar = tqdm(self.train_loader)
-        num_img_tr = len(self.train_loader)
-        for i, sample in enumerate(tbar):
-            if len(sample["image"]) > 1:
-                image, target = sample["image"], sample["label"]
-                if self.args.cuda:
-                    image, target = image.cuda(), target.cuda()
-                self.scheduler(self.optimizer, i, epoch, self.best_pred)
-                self.optimizer.zero_grad()
-                output = self.model(image)
-                loss = self.criterion(output, target)
-                loss.backward()
-                self.optimizer.step()
-                train_loss += loss.item()
-                tbar.set_description("Train loss: %.3f" % (train_loss / (i + 1)))
-                self.writer.add_scalar(
-                    "train/total_loss_iter", loss.item(), i + num_img_tr * epoch
-                )
-
-                # Show 10 * 3 inference results each epoch
-                if i % (num_img_tr // 10) == 0:
-                    global_step = i + num_img_tr * epoch
-                    self.summary.visualize_image(
-                        self.writer,
-                        self.args.dataset,
-                        image,
-                        target,
-                        output,
-                        global_step,
-                    )
-
-        self.writer.add_scalar("train/total_loss_epoch", train_loss, epoch)
-        print(
-            "[Epoch: %d, numImages: %5d]"
-            % (epoch, i * self.args.batch_size + image.data.shape[0])
-        )
-        print(f"Loss: {train_loss:.3f}")
-
-        if self.args.no_val:
-            # save checkpoint every epoch
-            is_best = False
-            self.saver.save_checkpoint(
-                {
-                    "epoch": epoch + 1,
-                    "state_dict": self.model.module.state_dict(),
-                    "optimizer": self.optimizer.state_dict(),
-                    "best_pred": self.best_pred,
-                },
-                is_best,
-            )
+        do_training(self, epoch)
 
     def validation(self, epoch):
         self.model.eval()
